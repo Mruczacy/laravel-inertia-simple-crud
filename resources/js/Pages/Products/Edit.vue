89bcb758718928809dc6541ac1576cada195a8c1
Mruczący Von Mruczek <mruczacy-kontakt@wp.pl>
2022-07-21 17:17:46 +0200

    Fixed updating

--- resources/js/Pages/Products/Edit.vue
+++ resources/js/Pages/Products/Edit.vue
@@ -17,10 +17,10 @@ export default {
     data() {
         return {
             form: this.$inertia.form({
-                name: null,
-                base_price: null,
-                discount_price: null,
-                description: null,
+                name: this.product.name,
+                base_price: this.product.base_price,
+                discount_price: this.product.discount_price,
+                description: this.product.description,
             }),
             errors: null,
         }
@@ -46,10 +46,10 @@ export default {
         </div>
         <form @submit.prevent="updateProduct(product)">
             <div class="flex flex-wrap -mb-8 -mr-6 p-8">
-                <text-input v-model="product.name" :error="form.errors.name" class="pb-8 pr-6 w-full lg:w-1/2" label="Name" />
-                <text-input v-model="product.base_price" :error="form.errors.base_price" class="pb-8 pr-6 w-full lg:w-1/2" label="Base Price" />
-                <text-input v-model="product.discount_price" :error="form.errors.discount_price" class="pb-8 pr-6 w-full lg:w-1/2" label="Discount Price" />
-                <text-input v-model="product.description" :error="form.errors.description" class="pb-8 pr-6 w-full lg:w-1/2" label="Description" />
+                <text-input v-model="form.name" :error="form.errors.name" class="pb-8 pr-6 w-full lg:w-1/2" label="Name" />
+                <text-input v-model="form.base_price" :error="form.errors.base_price" class="pb-8 pr-6 w-full lg:w-1/2" label="Base Price" />
+                <text-input v-model="form.discount_price" :error="form.errors.discount_price" class="pb-8 pr-6 w-full lg:w-1/2" label="Discount Price" />
+                <text-input v-model="form.description" :error="form.errors.description" class="pb-8 pr-6 w-full lg:w-1/2" label="Description" />
             </div>
             <div class="flex items-center justify-end px-8 py-4 bg-gray-50 border-t border-gray-100">
                 <loading-button :loading="form.processing" class="bg-indigo-500 hover:bg-indigo-700 rounded font-bold text-white" type="submit">Edit Product</loading-button>
