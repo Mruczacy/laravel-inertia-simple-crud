f53190112db8208409453af06d483eb81c61b174
Mruczący Von Mruczek <mruczacy-kontakt@wp.pl>
2022-07-21 16:56:28 +0200

    Added database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,24 +1,24 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
+    public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'name' => env('APP_TEST_USERNAME'),
+            'email' => env('APP_TEST_EMAIL'),
+            'password' => bcrypt(env('APP_TEST_PASSWORD')),
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        ]);
+
+        Product::factory()->create(10);
     }
 }
